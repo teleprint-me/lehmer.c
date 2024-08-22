a636d6dc0424bebc5c62a4f53f5380cb2e933d7c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-22 14:55:04 -0400

    tests(api): Refactor and add detailed comments to attempt to isolate the issue

--- tests/test_lehmer_api.c
+++ tests/test_lehmer_api.c
@@ -19,31 +19,62 @@
 int test_lehmer_api(void) {
     bool passed = true;
 
-    uint64_t state_value;
-    double   random_value;
+    double random_value;
 
-    lehmer_state_t* state = lehmer_create_state(STREAMS);
+    /* setup */
 
-    // Test the default stream (stream 0)
-    lehmer_select_stream(state, 0); // Select the default stream
-    lehmer_seed_streams(state, 1);  // Set the initial seed for stream 0
-    for (long i = 0; i < 10000; i++) {
-        random_value = lehmer_generate(state); // Generate random values
+    // Initialize the state using the predefined number of streams
+    lehmer_state_t* state = lehmer_create_state(STREAMS); // STREAMS = 256
+    // Set the initial state to seed the streams using the default value
+    lehmer_seed_streams(state, DEFAULT); // DEFAULT = 123456789
+
+    /* initialization */
+
+    // Select the default stream
+    lehmer_select_stream(state, 0); // first stream
+    // Set the initial seed for stream 0
+    lehmer_set_seed(state, 1); // value is 1
+
+    /* generation */
+
+    for (size_t i = 0; i < 10000; i++) {
+        // Generate random values
+        random_value = lehmer_generate(state);
     }
-    state_value = lehmer_get_seed(state); // Retrieve the final state value
-    passed      = (state_value == CHECK); // Validate the state
+
+    /* validation */
+
+    /* test 1: validate number generation */
+
+    // Retrieve the final state value
+    uint64_t state_value = lehmer_get_seed(state);
+    // Validate the state
+    passed               = (state_value == CHECK);
+
+    // Output test 1 results
+    if (passed) {
+        printf("PASS: test_lehmer_generate_api: number generation\n");
+    } else {
+        printf("FAIL: test_lehmer_generate_api: number generation\n");
+    }
+
+    /* test 2: validate jump states */
 
     // Test stream 1
-    lehmer_select_stream(state, 1); // Select stream 1
-    lehmer_seed_streams(state, 1);  // Set the initial seeds for all streams
-    state_value = lehmer_get_seed(state); // Retrieve the state of stream 1
-    passed      = passed && (state_value == A256); // Validate the state
+    lehmer_select_stream(state, 1); // second stream
+    // reset the streams by reseeding them with a value of 1
+    lehmer_seed_streams(state, 1);
+    // Retrieve the state of stream 1
+    state_value = lehmer_get_seed(state);
+    // Validate the state; note, previous test affects current test
+    // tests should be isolated/encapsulated
+    passed      = passed && (state_value == A256);
 
     // Output the result
     if (passed) {
-        printf("PASS: test_lehmer_generate_api\n");
+        printf("PASS: test_lehmer_generate_api: jump stream\n");
     } else {
-        printf("FAIL: test_lehmer_generate_api\n");
+        printf("FAIL: test_lehmer_generate_api: jump stream\n");
     }
 
     lehmer_free_state(state);
