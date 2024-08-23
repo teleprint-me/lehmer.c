82655eb2e29d3d9d8c87c70aa0e60cf1e969686a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-23 13:02:15 -0400

    tests(period): Refactor and streamline the full period test for integration

--- tests/test_lehmer_period.c
+++ tests/test_lehmer_period.c
@@ -3,14 +3,24 @@
  *
  * @file tests/test_lehmer_period.c
  *
- * @brief Concept for testing a full period of the Lehmer RNG
+ * @brief Proof-of-Concept for testing a full period of the Lehmer RNG
+ *
+ * @note This method can be sped up by applying asynchronous calculations as
+ *       this is rather computationally expensive, even in isolation.
  */
+
+#include "lehmer.h"
 
 #include <stdbool.h>
 #include <stdio.h>
 
-#define MODULUS    2147483647
-#define MULTIPLIER 16807
+#ifndef MODULUS
+    #define MODULUS 2147483647
+#endif
+
+#ifndef MULTIPLIER
+    #define MULTIPLIER 16807
+#endif
 
 long lehmer_rng(long seed) {
     return (seed * MULTIPLIER) % MODULUS;
@@ -20,30 +30,29 @@ long lehmer_rng(long seed) {
  * @brief This test checks if the sequence returns to the initial seed
  * (z_1 = 1) only after (m - 1) iterations, confirming the full period.
  */
-void test_full_period() {
+int test_full_period(void) {
     bool passed = true;
 
+    long count         = 0;
     long seed          = 1;
     long original_seed = seed;
-    int  count         = 0;
 
     do {
         seed = lehmer_rng(seed);
         count++;
     } while (seed != original_seed && count < MODULUS - 1);
 
-    if (count == MODULUS - 1) {
-        printf("Test passed: Full period detected.\n");
+    passed = count == (MODULUS - 1);
+
+    if (passed) {
+        printf("PASS: Full period detected.\n");
     } else {
-        printf(
-            "Test failed: Period detected was %d, which is less than MODULUS "
-            "- 1.\n",
-            count
-        );
+        printf("FAIL: %d is less than %d - 1.\n", count, MODULUS);
     }
+
+    return (passed) ? 0 : 1;
 }
 
 int main() {
-    test_full_period();
-    return 0;
+    return test_full_period();
 }
