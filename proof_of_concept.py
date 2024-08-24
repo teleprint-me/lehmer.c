d29e23370cf631f851ac5b15e88a0040d4f49f7e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-23 20:33:03 -0400

    lehmer(python): Refactor to test hypothesis in application

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -14,8 +14,20 @@ m is the modulus
 algorithm is the same for int and float
 """
 
+
+def is_prime(n):
+    if n < 2:
+        return False
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+
 class Lehmer:
-    MODULUS = 2**63-1  # Must be a large prime integer
+    MODULUS = 2**63 - 1  # Must be a large prime integer
     MULTIPLIER = 48271
     A256 = 22925
 
@@ -27,8 +39,15 @@ class Lehmer:
         return self.seed
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    print(f"2^31-1 is prime? {is_prime(2**31-1)}")
+    print(f"2^63-1 is prime? {is_prime(2**63-1)}")  # Corrected typo here
+
     # Testing the generator
     lehmer = Lehmer(seed=123456789)
+    print(f"MODULUS is prime? {is_prime(lehmer.MODULUS)}")
+    print(f"MULTIPLIER is prime? {is_prime(lehmer.MULTIPLIER)}")
+
     output = [lehmer.lehmer_generator() for _ in range(10)]
-    print(output)
+    for element in output:
+        print(element)
