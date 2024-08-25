0a4723b8ae08565bd0a664b280afa1fb0f6107dc
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-25 19:39:41 -0400

    tests(generate): Refactor the tests to simplify and streamline them
    - Add use of print functions for state for debugging output
    - Fix docstring regarding state creation

--- tests/test_lehmer_generate.c
+++ tests/test_lehmer_generate.c
@@ -23,24 +23,27 @@
  * @note The state should always be deterministic which makes the LCG
  * mathematically and computationally sound. This seems counter-intuitive,
  * but is backed up by the maths. This is why it's called a Pseudo Random
- * Number Generator and not a Pure (or Real) Random Number Generator.
+ * Number Generator and not a Real Random Number Generator.
  * This is also why it's insecure for use within cryptographic operations.
- * If the seed is known, then any given sequence is known, making the output
- * predictable.
+ * If the parameters are known, then any given sequence is known, making the
+ * output predictable.
  */
 lehmer_state_t* setup_lehmer_state(void) {
     return lehmer_state_create(LEHMER_STREAMS, LEHMER_SEED);
 }
 
 bool validate_random_seed(lehmer_state_t* state, int64_t expected_seed) {
-    int64_t seed = lehmer_seed_get(state);
-    return seed == expected_seed;
+    for (size_t i = 0; i < 10000; i++) {
+        lehmer_generate_modulo(state);
+    }
+    int64_t current_seed = lehmer_seed_get(state);
+    return current_seed == expected_seed;
 }
 
 // Function to validate the number generation
 bool validate_random_value(lehmer_state_t* state, double expected_output) {
-    double random_output = lehmer_random_gamma(state);
-    return float_is_close(random_output, expected_output, /*significand*/ 6);
+    double current_value = lehmer_random_modulo(state);
+    return float_is_close(current_value, expected_output, /*significand*/ 6);
 }
 
 // Main test function
@@ -48,11 +51,16 @@ int test_lehmer_generate(void) {
     bool            passed = true;
     lehmer_state_t* state  = setup_lehmer_state();
 
-    passed = validate_random_value(state, /*expected_output*/ 0.053803);
-    if (passed) {
-        printf("PASS: test_lehmer_generate_value\n");
-    } else {
-        printf("FAIL: test_lehmer_generate_value\n");
+    passed = validate_random_seed(state, LEHMER_CHECK_SEED);
+    if (!passed) {
+        fprintf(stderr, "FAIL: validate_random_seed\n");
+        lehmer_state_print(state);
+    }
+
+    passed &= validate_random_value(state, LEHMER_CHECK_VALUE);
+    if (!passed) {
+        fprintf(stderr, "FAIL: validate_random_value\n");
+        lehmer_state_print(state);
     }
 
     lehmer_state_free(state);
