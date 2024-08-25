090511256b170c8f3aa8f2734fb978e8cae395f2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-08-25 04:02:16 -0400

    lehmer(tests): Add updates for testing lehmer rng

--- tests/test_lehmer_generate.c
+++ tests/test_lehmer_generate.c
@@ -25,40 +25,29 @@
  * predictable.
  */
 lehmer_state_t* setup_lehmer_state(void) {
-    lehmer_state_t* state = lehmer_create_state(STREAMS); // STREAMS = 256
-    lehmer_seed_streams(state, DEFAULT); // DEFAULT = 123456789
-    return state;
+    return lehmer_state_create(LEHMER_STREAMS, LEHMER_SEED);
 }
 
 // Function to validate the number generation
-bool validate_lehmer_generator(
-    lehmer_state_t* state, double expected_output, size_t significand
-) {
-    double random_output = lehmer_generate(state);
-    return float_is_close(random_output, expected_output, significand);
+bool validate_random_value(lehmer_state_t* state, double expected_output) {
+    double random_output = lehmer_random_gamma(state);
+    return float_is_close(random_output, expected_output, /*significand*/ 6);
 }
 
 // Main test function
 int test_lehmer_generate(void) {
-    bool passed = true;
+    bool            passed          = true;
+    double          expected_output = 0.053803; // magic value
+    lehmer_state_t* state           = setup_lehmer_state();
 
-    // 0.053803 is a deterministic value that is always produced
-    // when STREAMS = 256 and DEFAULT = 123456789
-    double expected_output = 0.053803;
-    size_t significand     = 6;
-
-    lehmer_state_t* state = setup_lehmer_state();
-
-    passed = validate_lehmer_generator(state, expected_output, significand);
-
-    // Output the result
+    passed = validate_random_value(state, expected_output);
     if (passed) {
         printf("PASS: test_lehmer_generate_value\n");
     } else {
         printf("FAIL: test_lehmer_generate_value\n");
     }
 
-    lehmer_free_state(state);
+    lehmer_state_free(state);
 
     return passed ? 0 : 1; // 0 on success, 1 on failure
 }
