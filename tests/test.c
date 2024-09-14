a4c16d3bfaf5d8836a8c6d42afdb60bf30627aec
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-14 16:05:44 -0400

    test(feat): Add initial draft for running test cases

--- tests/test.c
+++ tests/test.c
@@ -10,3 +10,28 @@
  */
 
 #include "test.h"
+
+#include <time.h>
+
+void test_case_run(test_case_t* test) {
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    test_meta_t meta
+        = {.state = TEST_RUNNING, .name = test->name, .elapsed_time_ns = 0};
+    test->function(&meta);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    meta.elapsed_time_ns
+        = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
+
+    if (meta.state == TEST_PASSED) {
+        LOG_INFO(
+            "Test '%s' PASSED in %lu ns", test->name, meta.elapsed_time_ns
+        );
+    } else {
+        LOG_ERROR(
+            "Test '%s' FAILED in %lu ns", test->name, meta.elapsed_time_ns
+        );
+    }
+}
