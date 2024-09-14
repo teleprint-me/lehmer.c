7feecb34e8696a4dd183f4fa40430e86e43650cb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-14 01:28:14 -0400

    tests(lehmer): Refactor test suite

--- tests/test_lehmer.c
+++ tests/test_lehmer.c
@@ -1,7 +1,7 @@
 /**
  * Copyright © 2024 Austin Berrio
  *
- * @file tests/test_lehmer_api.c
+ * @file tests/test_lehmer.c
  *
  * @brief A set of simple tests for validating selecting streams and seeding.
  */
@@ -16,26 +16,18 @@
 #include <stdlib.h>
 
 /**
- * @brief Function to setup the Lehmer RNG state
- *
- * @note The state should always be deterministic which makes the LCG
- * mathematically and computationally sound. This seems counter-intuitive,
- * but is backed up by the maths. This is why it's called a Pseudo Random
- * Number Generator and not a Real Random Number Generator.
- * This is also why it's insecure for use within cryptographic operations.
- * If the parameters are known, then any given sequence is known, making the
- * output predictable.
+ * @brief Creates a Lehmer RNG state with default parameters
  */
-lehmer_state_t* setup_lehmer_state(void) {
+lehmer_state_t* create_deterministic_lehmer_state(void) {
     return lehmer_state_create(LEHMER_STREAMS, LEHMER_SEED);
 }
 
 /**
- * @brief This test checks if the sequence returns to the initial seed
- * (z_1 = 1) only after (m - 1) iterations, confirming the full period.
+ * @brief Test if the Lehmer RNG produces a full period cycle, returning to the
+ * initial seed after a specific number of iterations.
  *
- * @brief Test for checking if the multiplier and modulus produce a full period
- *        output for the Lehmer RNG.
+ * This test checks if the sequence returns to the initial seed (z_1 = 1) only
+ * after (m - 1) iterations, confirming the full period.
  *
  * @note This method can be sped up by applying asynchronous calculations as
  *       this is rather computationally expensive, even in isolation.
@@ -58,23 +50,35 @@ int test_full_period(lehmer_state_t* state) {
 
     passed = count == (LEHMER_MODULUS - 1);
 
-    if (passed) {
-        printf("PASS: Full period detected.\n");
-    } else {
-        printf("FAIL: %d is less than %d - 1.\n", count, LEHMER_MODULUS);
+    if (!passed) {
+        LOG_ERROR("FAIL: %d is less than %d - 1.\n", count, LEHMER_MODULUS);
     }
 
+    printf("%s", passed ? "." : "x");
     return (passed) ? 0 : 1;
 }
 
+/**
+ * @brief Generate 10,000 random numbers and checks if the final seed matches
+ * the expected one.
+ *
+ * @return 0 on success and 1 on failure.
+ */
 int test_random_seed(lehmer_state_t* state, int32_t expected_seed) {
     for (size_t i = 0; i < 10000; i++) {
         lehmer_generate_modulo(state);
     }
     int32_t current_seed = lehmer_seed_get(state);
-    return current_seed == expected_seed ? 0 : 1;
+    bool    passed       = current_seed == expected_seed;
+
+    printf("%s", passed ? "." : "x");
+    return passed ? 0 : 1;
 }
 
+/**
+ * @brief Check if a generated random value is close to the given expected
+ * output.
+ */
 int test_random_value(lehmer_state_t* state, float expected_output) {
     float current_value = lehmer_seed_normalize(state);
     printf(
@@ -82,11 +86,16 @@ int test_random_value(lehmer_state_t* state, float expected_output) {
         expected_output,
         current_value
     );
-    return float_is_close(current_value, expected_output, /*significand*/ 6)
-               ? 0
-               : 1;
+    bool passed
+        = float_is_close(current_value, expected_output, /*significand*/ 6);
+
+    printf("%s", passed ? "." : "x");
+    return passed ? 0 : 1;
 }
 
+/**
+ * @brief Test the seed generation process for a specific stream and seed.
+ */
 int test_seed_generation(lehmer_state_t* state, uint32_t expected_seed) {
     // stream is set to 0
     lehmer_state_select(state, 0);
@@ -100,10 +109,15 @@ int test_seed_generation(lehmer_state_t* state, uint32_t expected_seed) {
 
     // get the current seed from stream 0
     uint32_t current_seed = lehmer_seed_get(state);
-    // assert valid seed
-    return (current_seed == expected_seed) ? 0 : 1;
+    bool     passed       = (current_seed == expected_seed);
+
+    printf("%s", passed ? "." : "x");
+    return passed ? 0 : 1;
 }
 
+/**
+ * @brief Test the jump to a specific stream and seed.
+ */
 int test_jump_state(lehmer_state_t* state, uint32_t expected_seed) {
     // stream is set to 1
     lehmer_state_select(state, 1);
@@ -111,34 +125,36 @@ int test_jump_state(lehmer_state_t* state, uint32_t expected_seed) {
     lehmer_seed_streams(state, 1);
     // get the current seed from stream 1
     uint32_t current_seed = lehmer_seed_get(state);
-    // assert valid seed
-    return (current_seed == expected_seed) ? 0 : 1;
+    bool     passed       = (current_seed == expected_seed);
+
+    printf("%s", passed ? "." : "x");
+    return passed ? 0 : 1;
 }
 
-// Main test function
-int test_lehmer_api(void) {
-    bool            passed = true;
-    lehmer_state_t* state  = setup_lehmer_state();
+int main(void) {
+    // both shell and cmake expect 0 on success and 1 on failure
+    int             passed = 0; // Assuming success
+    lehmer_state_t* state  = create_deterministic_lehmer_state();
 
-    passed = validate_seed_generation(state, LEHMER_CHECK_JUMP);
+    passed |= validate_seed_generation(state, LEHMER_CHECK_JUMP);
     if (!passed) {
         LOG_ERROR("FAIL: validate_seed_generation\n");
         lehmer_state_print(state); // print state to stderr for debugging
     }
 
-    passed &= validate_jump_state(state, LEHMER_JUMP);
+    passed |= validate_jump_state(state, LEHMER_JUMP);
     if (!passed) {
         LOG_ERROR("FAIL: validate_jump_state\n");
         lehmer_state_print(state);
     }
 
-    passed = validate_random_seed(state, LEHMER_CHECK_SEED);
+    passed |= validate_random_seed(state, LEHMER_CHECK_SEED);
     if (!passed) {
         LOG_ERROR("FAIL: validate_random_seed\n");
         lehmer_state_print(state);
     }
 
-    passed &= validate_random_value(state, LEHMER_CHECK_VALUE);
+    passed |= validate_random_value(state, LEHMER_CHECK_VALUE);
     if (!passed) {
         LOG_ERROR("FAIL: validate_random_value\n");
         lehmer_state_print(state);
@@ -147,9 +163,4 @@ int test_lehmer_api(void) {
     lehmer_state_free(state);
 
     return (passed) ? 0 : 1;
-}
-
-int main(void) {
-    // both shell and cmake expect 0 on success and 1 on failure
-    return test_lehmer_api();
 }
