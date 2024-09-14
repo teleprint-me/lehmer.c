d126d68ea2e3e70a1c8cfa49740eb6f57ab298c6
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-14 14:12:28 -0400

    test(feat): Add macros for comparing string and float values
    - Add initial sketch for supporting data types
    - Add helper function for checking data type

--- tests/test.c
+++ tests/test.c
@@ -3,11 +3,15 @@
  *
  * @file test.c
  *
- * @brief A simple toolkit for writing test suites
+ * @brief A simple toolkit for writing test suites.
+ * - Readability above all else.
+ * - Pragmatism throughout.
+ * - Kept simple at heart.
  */
 
 // Includes
 
+#include "float_is_close.h" // *_is_close(); float_ or double_
 #include "logger.h" // LOG_ERROR, LOG_INFO, LOG_*
 
 #include <stdint.h>
@@ -24,7 +28,39 @@
         } \
     } while (0)
 
+#define ASSERT_EQUAL_STRING(actual, expected, meta) \
+    do { \
+        if (strcmp((actual), (expected)) != 0) { \
+            LOG_ERROR("Test '%s' failed: %s", (meta)->name, (meta)->message); \
+            (meta)->state = TEST_FAILED; \
+        } else { \
+            (meta)->state = TEST_PASSED; \
+        } \
+    } while (0)
+
+#define ASSERT_EQUAL_FLOAT(actual, expected, significand, meta) \
+    do { \
+        if (float_is_close(actual, expected, significand)) { \
+            LOG_ERROR("Test '%s' failed: %s", (meta)->name, (meta)->message); \
+            (meta)->state = TEST_FAILED; \
+        } else { \
+            (meta)->state = TEST_PASSED; \
+        } \
+    } while (0)
+
 // Aliases
+
+// @note Order should not matter with enumerations - but I understand why there
+// may be edge cases which depend upon the context.
+
+// @note Consider automating this with a user defined function.
+// This gets messy fast.
+typedef enum TestDataType {
+    TEST_TYPE_INTEGER = 0,
+    TEST_TYPE_FLOAT   = 1,
+    TEST_TYPE_STRING  = 2,
+    // Add support for struct or complex types later
+} test_data_t;
 
 typedef enum TestState {
     TEST_PASSED  = 0x1,
@@ -35,7 +71,7 @@ typedef enum TestState {
 } test_state_t;
 
 // @note Consider refactoring if this becomes bloated.
-// Avoid "The Blob" at all costs, or face your own peril.
+// Avoid "The Blob" at all costs, or become absorbed by your own peril.
 typedef struct TestMetaData {
     test_state_t state;
     const char*  name;
@@ -46,8 +82,9 @@ typedef struct TestMetaData {
 } test_meta_t; // e.g. data->state
 
 typedef struct TestParameter {
-    void**   cases; // Array of void pointers for each test case
-    uint32_t size; // Number of parameters
+    void**      cases; // Array of void pointers for each test case
+    uint32_t    size; // Number of parameters
+    test_data_t type; // Type descriptor
 } test_parameter_t; // e.g. parameter->cases
 
 typedef void (*test_callback_t)(test_meta_t* data);
@@ -56,3 +93,9 @@ typedef struct {
     const char*     name;
     test_callback_t function;
 } test_case_t;
+
+// Prototypes
+
+// @note A helper function for type checking might make this even more concise,
+// so we don’t repeat checks throughout.
+int test_data_type_check(test_parameter_t* param, test_data_t expected_type);
