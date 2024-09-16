106ceef5e416826fa86d5b0dd800d04b996db5ad
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-16 03:29:52 -0400

    examples(feat): Add a simple implementation for using the core Lehmer API

---
+++ examples/impl.c
@@ -0,0 +1,46 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file examples/lehmer.c
+ *
+ * @brief Minimal implementation
+ */
+
+#include "lehmer.h"
+#include "logger.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+#define MAX_SEEDS 10
+
+int32_t expected_seeds[MAX_SEEDS] = {
+    115541394,
+    283598515,
+    1523151587,
+    652633738,
+    1845549155,
+    291648857,
+    1426670162,
+    1289797906,
+    2136310349,
+    1819611286,
+};
+
+int main(void) {
+    lehmer_state_t* state = lehmer_state_create(LEHMER_STREAMS, LEHMER_SEED);
+
+    for (uint32_t i = 0; i < MAX_SEEDS; i++) {
+        lehmer_generate_modulo(state);
+        int32_t current  = lehmer_seed_get(state);
+        int32_t expected = expected_seeds[i];
+        printf("Iteration %u: Expected %d, Got %d\n", i, expected, current);
+        assert(expected == current);
+    }
+
+    float output = lehmer_seed_normalize(state);
+    printf("Normalized Seed: %.9f\n", output);
+
+    lehmer_state_free(state);
+    return 0;
+}
