b2f50039836250dc4f2c02316c4ac743f7f7e917
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-16 15:26:03 -0400

    examples(refactor): Convert impl into a flexible CLI tool for debugging

--- examples/impl.c
+++ examples/impl.c
@@ -9,7 +9,9 @@
 #include "lehmer.h"
 #include "logger.h"
 
+#include <getopt.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define MAX_SEEDS 10
 
@@ -40,22 +42,87 @@ int32_t expected_stream_0[MAX_SEEDS] = {
         exit(1); \
     }
 
-int test_lehmer_generator_modulo(
-    lehmer_state_t* state, int32_t* values, uint32_t stream
-) {
+int test_lehmer_generator_modulo(lehmer_state_t* state, int32_t* expected) {
     for (uint32_t i = 0; i < MAX_SEEDS; i++) {
-        lehmer_state_select(state, stream);
         lehmer_generate_modulo(state);
-        int32_t current  = lehmer_seed_get(state);
-        int32_t expected = values[i];
-        printf("Iteration %u: Expected %d, Got %d\n", i, expected, current);
-        LEHMER_ASSERT(i, expected, current);
+        int32_t current = lehmer_seed_get(state);
+        printf("Iteration %u: Expected %d, Got %d\n", i, expected[i], current);
+        LEHMER_ASSERT(i, expected[i], current);
     }
 }
 
-int main(void) {
+void print_usage(char* argv[]) {
+    fprintf(stderr, "Usage:\n");
+    fprintf(
+        stderr,
+        "\t%s [--help] "
+        "[--verbose <0|1>] "
+        "[--test <0|1>] "
+        "[--stream <unsigned integer>]\n",
+        argv[0]
+    );
+}
+
+// Define command line option structure
+static struct option long_options[]
+    = {{"help", optional_argument, 0, 'h'},
+       {"verbose", required_argument, 0, 'v'},
+       {"test", required_argument, 0, 't'},
+       {"stream", required_argument, 0, 's'},
+       {0, 0, 0, 0}};
+
+int main(int argc, char* argv[]) {
+    uint32_t stream = 0; // default stream is 0
+    bool     test = false;
+    bool     verbose = false;
+
+    while (true) {
+        static int opt_index = 0;
+
+        int c = getopt_long(argc, argv, "h:s:n:m:", long_options, &opt_index);
+
+        if (c == -1) {
+            break;
+        }
+
+        switch (c) {
+            case 'h':
+                print_usage(argv);
+                return EXIT_SUCCESS;
+            case 'v':
+                // Output state every iteration
+                verbose = (bool) atoi(optarg) ? true : false;
+                break;
+            case 't':
+                // Enable testing initial stream
+                test = (bool) atoi(optarg) ? true : false;
+                break;
+            case 's':
+                // Convert seed input
+                stream = (uint32_t) atoi(optarg);
+                printf("Setting stream to: %d\n", stream);
+                break;
+            default:
+                print_usage(argv);
+                return EXIT_FAILURE;
+        }
+    }
+
     lehmer_state_t* state = lehmer_state_create(LEHMER_STREAMS, LEHMER_SEED);
-    test_lehmer_generator_modulo(state, expected_stream_0, 0);
+    lehmer_state_select(state, stream);
+    if (test) {
+        test_lehmer_generator_modulo(state, expected_stream_0);
+    } else {
+        for (uint32_t i = 0; i < MAX_SEEDS; i++) {
+            lehmer_generate_modulo(state);
+            if (verbose) {
+                lehmer_state_print(state);
+            } else {
+                int32_t current = lehmer_seed_get(state);
+                printf("Iteration %u: Seed %d\n", i, current);
+            }
+        }
+    }
 
     float output = lehmer_seed_normalize(state);
     printf("Normalized Seed: %.9f\n", output);
