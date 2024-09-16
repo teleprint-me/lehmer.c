5d45d6e82ee9baeac167a1f8494eb8efdc342f2e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-16 04:37:59 -0400

    examples(impl): Refactor to generalize interface

--- examples/impl.c
+++ examples/impl.c
@@ -13,7 +13,10 @@
 
 #define MAX_SEEDS 10
 
-int32_t expected_seeds[MAX_SEEDS] = {
+// @brief Valid stream for selected state at stream 0 using LEHMER_SEED
+// @ref python -m lehmer.cli -v -i 10 -r 0
+// `python -m lehmer.cli -h` for more info
+int32_t expected_stream_0[MAX_SEEDS] = {
     115541394,
     283598515,
     1523151587,
@@ -37,12 +40,14 @@ int32_t expected_seeds[MAX_SEEDS] = {
         exit(1); \
     }
 
-int test_lehmer_generator_modulo(lehmer_state_t* state) {
+int test_lehmer_generator_modulo(
+    lehmer_state_t* state, int32_t* values, uint32_t stream
+) {
     for (uint32_t i = 0; i < MAX_SEEDS; i++) {
-        lehmer_state_select(state, 0);
+        lehmer_state_select(state, stream);
         lehmer_generate_modulo(state);
         int32_t current  = lehmer_seed_get(state);
-        int32_t expected = expected_seeds[i];
+        int32_t expected = values[i];
         printf("Iteration %u: Expected %d, Got %d\n", i, expected, current);
         LEHMER_ASSERT(i, expected, current);
     }
@@ -50,7 +55,7 @@ int test_lehmer_generator_modulo(lehmer_state_t* state) {
 
 int main(void) {
     lehmer_state_t* state = lehmer_state_create(LEHMER_STREAMS, LEHMER_SEED);
-    test_lehmer_generator_modulo(state);
+    test_lehmer_generator_modulo(state, expected_stream_0, 0);
 
     float output = lehmer_seed_normalize(state);
     printf("Normalized Seed: %.9f\n", output);
