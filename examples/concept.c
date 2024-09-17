1e6a40a4b6546a9c5aebe60cbc7a76998ac10025
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-17 00:59:15 -0400

    examples(concept): Add macros, use 32-bit int precision, and some other stuff
    - Apply new formatting rule for aligned macros
    - Add macros for handling sane default values
    - Simplify expression for handling generated seed output
    - Enforce use of 32-bit integer precision
    - Add generator for jump scaling
    - Add error handling for invalid time_t values
    - Add function for handling seed normalization

--- examples/concept.c
+++ examples/concept.c
@@ -13,36 +13,41 @@
 /**
  * @param LEHMER_MODULUS Mersenne prime number used as modulus (2^31 - 1).
  */
-#define LEHMER_MODULUS     2147483647
+#define LEHMER_MODULUS 2147483647
 
 /**
  * @param LEHMER_MULTIPLIER Prime number used to scale the seed.
  */
-#define LEHMER_MULTIPLIER  48271
+#define LEHMER_MULTIPLIER 48271
 
 /**
  * @param LEHMER_JUMP Prime number used as a multiplier to "jump" scale seeds.
  */
-#define LEHMER_JUMP        22937
+#define LEHMER_JUMP 22937
+
+/**
+ * @param LEHMER_SIZE Default allocation size for a given number of seeds
+ */
+#define LEHMER_SIZE 256
+
+/**
+ * @param LEHMER_SEED Default seed value
+ */
+#define LEHMER_SEED 123456789
 
 /**
  * @param LEHMER_QUOTIENT Result of dividing LEHMER_MODULUS by
  * LEHMER_MULTIPLIER. Useful for range normalization in Lehmer-based
  * calculations.
  */
-#define LEHMER_QUOTIENT    ((LEHMER_MODULUS) / (LEHMER_MULTIPLIER))
+#define LEHMER_QUOTIENT ((LEHMER_MODULUS) / (LEHMER_MULTIPLIER))
 
 /**
  * @param LEHMER_REMAINDER Result of modulus operation between LEHMER_MODULUS
  * and LEHMER_MULTIPLIER. Important for handling overflows and adjusting
  * outputs.
  */
-#define LEHMER_REMAINDER   ((LEHMER_MODULUS) % (LEHMER_MULTIPLIER))
-
-/**
- * @param LEHMER_STREAM_SIZE Default number of streams
- */
-#define LEHMER_STREAM_SIZE 256
+#define LEHMER_REMAINDER ((LEHMER_MODULUS) % (LEHMER_MULTIPLIER))
 
 // @note the size of seed and stream must be the same
 // the general idea is that we select a seed
@@ -54,7 +59,7 @@ typedef struct LehmerState {
 } lehmer_state_t;
 
 static inline int32_t lehmer_generate_output(int32_t val, uint32_t mod) {
-    return (val > 0) ? val : (val + mod) % mod;
+    return (int32_t) ((val + mod) % mod);
 }
 
 void lehmer_generate_modulo(lehmer_state_t* state) {
@@ -64,16 +69,16 @@ void lehmer_generate_modulo(lehmer_state_t* state) {
     uint32_t i = state->index;
     int32_t z = state->seed[i];
 
-    int32_t r = (int64_t) (a * z) % m;
+    int32_t r = (int32_t) (a * z) % m;
     state->seed[i] = lehmer_generate_output(r, m);
 }
 
 // Generates a sequence using gamma approach
 void lehmer_generate_gamma(lehmer_state_t* state) {
-    const uint64_t a = LEHMER_MULTIPLIER;
-    const uint64_t m = LEHMER_MODULUS;
-    const uint64_t r = LEHMER_REMAINDER;
-    const uint64_t q = LEHMER_QUOTIENT;
+    const uint32_t a = LEHMER_MULTIPLIER;
+    const uint32_t m = LEHMER_MODULUS;
+    const uint32_t q = LEHMER_QUOTIENT;
+    const uint32_t r = LEHMER_REMAINDER;
 
     uint32_t i = state->index;
     int32_t z = state->seed[i];
@@ -85,8 +90,8 @@ void lehmer_generate_gamma(lehmer_state_t* state) {
 
 // Generates a sequence using delta approach
 void lehmer_generate_delta(lehmer_state_t* state) {
-    const uint64_t a = LEHMER_MULTIPLIER;
-    const uint64_t m = LEHMER_MODULUS;
+    const uint32_t a = LEHMER_MULTIPLIER;
+    const uint32_t m = LEHMER_MODULUS;
     const uint32_t q = LEHMER_QUOTIENT;
 
     uint32_t i = state->index;
@@ -95,6 +100,19 @@ void lehmer_generate_delta(lehmer_state_t* state) {
     // @todo Validate the order of operations
     int32_t d = (int32_t) ((z / q) - (a * (z / m))); // delta
     state->seed[i] = lehmer_generate_output(d, m);
+}
+
+void lehmer_generate_jump(lehmer_state_t* state) {
+    const uint32_t a = LEHMER_JUMP;
+    const uint32_t m = LEHMER_MODULUS;
+    const uint32_t q = LEHMER_QUOTIENT;
+    const uint32_t r = LEHMER_REMAINDER;
+
+    uint32_t i = state->index;
+    int32_t z = state->seed[i];
+
+    int32_t j = (int32_t) ((a * (z % q)) - (r * (z / q)));
+    state->seed[i] = lehmer_generate_output(j, m);
 }
 
 // Generalized Lehmer sequence generator
@@ -117,7 +135,11 @@ void lehmer_generate_time(
 ) {
     // Get the current time as seed
     time_t now = time(NULL);
-    lehmer_generate(state, generate, (int32_t) now);
+    if (-1 == now) {
+        lehmer_generate(state, generate, (int32_t) now);
+    } else {
+        lehmer_generate(state, generate, (int32_t) LEHMER_SEED);
+    }
 }
 
 // Create and initialize the state with dynamic stream handling
@@ -128,10 +150,10 @@ lehmer_state_t* lehmer_state_create(uint32_t size, int32_t seed) {
         return NULL;
     }
 
-    // Initialize the index
+    // Initialize the index to 0
     state->index = 0;
-    // Ensure size is at least 1
-    state->size = (size == 0) ? 1 : size;
+    // Default to a size of 256 if size is 0
+    state->size = (size == 0) ? LEHMER_SIZE : size;
 
     // Allocate memory for generating seeds
     state->seed = (int32_t*) malloc(sizeof(int32_t) * state->size);
@@ -180,4 +202,7 @@ void lehmer_seed_set(lehmer_state_t* state, int32_t value) {
     lehmer_generate(state, lehmer_generate_modulo, seed);
 }
 
-// Placeholder for stream skipping functionality via LEHMER_JUMP
+// @note: generates a number in the range 0.0 to 1.0
+float lehmer_seed_normalize(lehmer_state_t* state) {
+    return (float) state->seed[state->index] / (float) LEHMER_MODULUS;
+}
