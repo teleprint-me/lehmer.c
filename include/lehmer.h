99bab91b95b4b77d9bcc97c468d7c0f250d96fd2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-18 01:09:01 -0400

    lehmer(patch): Remove static inline to allow API calls

--- include/lehmer.h
+++ include/lehmer.h
@@ -213,7 +213,7 @@ void lehmer_seed_regenerate(
  *
  * @return The normalized seed as a float in the range 0.0 to 1.0
  */
-static inline float lehmer_seed_normalize_to_float(lehmer_state_t* state);
+float lehmer_seed_normalize_to_float(lehmer_state_t* state);
 
 /**
  * @brief Normalizes a seed to an integer in the range 0 to m - 1
@@ -223,8 +223,7 @@ static inline float lehmer_seed_normalize_to_float(lehmer_state_t* state);
  *
  * @return The normalized seed as an integer in the range 0 to m - 1
  */
-static inline int32_t
-lehmer_seed_normalize_to_int(int32_t value, uint32_t modulus);
+int32_t lehmer_seed_normalize_to_int(int32_t value, uint32_t modulus);
 
 // Generate related functions
 
