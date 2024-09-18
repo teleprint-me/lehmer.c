9d88202926424555a2b774e2f8b4f6c252960297
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-17 22:05:39 -0400

    lehmer(docs): Add and refine docstrings

--- include/lehmer.h
+++ include/lehmer.h
@@ -34,15 +34,15 @@
 /**
  * @param LEHMER_MODULUS Mersenne prime number used as modulus (2^31 - 1).
  *
- * m: A large prime number
+ * m: A large Mersenne prime number appropriate for a given bit width.
  *
- * @note Must be a mersenne prime number
+ * @note Must be a mersenne prime number.
  * - 32-bit: 2^31 - 1 = 2147483647
  * - 64-bit: 2^61 - 1 = 2305843009213693951
  *
- * @note Originally chosen for the IBM System/360 to avoid overflow errors
- *       in 32-bit integer arithmetic. This choice remains relevant for
- *       modern embedded systems with similar constraints.
+ * @note Originally chosen for the IBM System/360 to avoid overflow errors in
+ * 32-bit integer arithmetic. This choice remains relevant for modern embedded
+ * systems with similar constraints.
  */
 #define LEHMER_MODULUS 2147483647
 
@@ -51,14 +51,13 @@
  *
  * a: A smaller prime number in the range 2, ..., m - 1
  *
- * @note Must be a prime number
+ * @note Must be a prime number.
  *
- * @note The multiplier was specifically selected to prevent overflow errors
- *       on hardware with 32-bit integer limitations. Despite being dated,
- *       this choice is still effective in modern contexts where 32-bit
- *       constraints apply.
+ * @note The multiplier was specifically selected to prevent overflow errors on
+ * hardware with 32-bit integer limitations. Despite being dated, this choice
+ * is still effective in modern contexts where 32-bit constraints apply.
  *
- * @warning 16807 was originally chosen for safe 32-bit arithmetic
+ * @warning 16807 was originally chosen for safe 32-bit arithmetic.
  */
 #define LEHMER_MULTIPLIER 48271
 
@@ -70,16 +69,16 @@
 #define LEHMER_JUMP 22937
 
 /**
- * @param LEHMER_SIZE Default allocation size for a given number of seeds
+ * @param LEHMER_SIZE Default allocation size for a given number of seeds.
  */
 #define LEHMER_SIZE 256
 
 /**
- * @param LEHMER_SEED Default seed value
+ * @param LEHMER_SEED Default seed value.
  *
  * z: A value between 0, ..., m - 1
  *
- * @note May be any value within the defined range
+ * @note May be any value within the defined range.
  */
 #define LEHMER_SEED 123456789
 
@@ -98,7 +97,7 @@
 #define LEHMER_REMAINDER ((LEHMER_MODULUS) % (LEHMER_MULTIPLIER))
 
 /**
- * @brief Structure representing the state of the LCG RNG
+ * @brief Structure representing the state of the LCG RNG.
  *
  * @param seed Array of seeds representing the initial state
  * @param size The number of seeds (upper limit)
@@ -119,26 +118,103 @@ typedef struct LehmerState {
  * -> lehmer_state_create
  */
 
+/**
+ * @brief Callback function for generating new seeds in the Lehmer RNG.
+ *
+ * @param state The current state object.
+ */
 typedef void (*lehmer_generate_t)(lehmer_state_t*);
 
 // Lehmer state management
 
+/**
+ * @brief Create and initialize the Lehmer RNG state with dynamic seed
+ * generation.
+ *
+ * @param size The number of seeds (upper limit).
+ * @param seed The initial seed value.
+ *
+ * @return A newly allocated and initialized Lehmer RNG state object or NULL if
+ * memory allocation fails.
+ */
 lehmer_state_t* lehmer_state_create(uint32_t size, int32_t seed);
+
+/**
+ * @brief Free the allocated memory for the Lehmer RNG state object.
+ *
+ * @param state The Lehmer RNG state object to be freed.
+ */
 void lehmer_state_free(lehmer_state_t* state);
+
+/**
+ * @brief Print the Lehmer RNG state object.
+ *
+ * @param state The Lehmer RNG state object to be printed.
+ */
 void lehmer_state_print(lehmer_state_t* state);
 
 // Lehmer seed management
 
+/**
+ * @brief Select a generated seed from the current state at a given index.
+ *
+ * @param state The Lehmer RNG state object.
+ * @param index The index of the seed to select.
+ *
+ * @note The index is bound to the allocated range (0 to size - 1).
+ */
 void lehmer_seed_select(lehmer_state_t* state, uint32_t index);
+
+/**
+ * @brief Get the currently selected seed from the Lehmer RNG state object.
+ *
+ * @param state The Lehmer RNG state object.
+ *
+ * @return The currently selected seed.
+ */
 int32_t lehmer_seed_get(lehmer_state_t* state);
+
+/**
+ * @brief Set the value of the currently selected seed in the Lehmer RNG state
+ * object.
+ *
+ * @param state The Lehmer RNG state object.
+ * @param value The new value for the selected seed.
+ *
+ * @note The value is bound to the modulus (0 to m - 1).
+ */
 void lehmer_seed_set(lehmer_state_t* state, int32_t value);
+
+/**
+ * @brief Regenerate a new sequence of seeds based on the currently selected
+ * seed.
+ *
+ * @param state The Lehmer RNG state object.
+ * @param generator The Lehmer RNG generation function to use.
+ */
 void lehmer_seed_regenerate(
     lehmer_state_t* state, lehmer_generate_t generator
 );
 
 // Lehmer seed normalization
 
+/**
+ * @brief Normalizes a seed to a float in the range 0.0 to 1.0
+ *
+ * @param state The Lehmer RNG state object.
+ *
+ * @return The normalized seed as a float in the range 0.0 to 1.0
+ */
 float lehmer_seed_normalize_to_float(lehmer_state_t* state);
+
+/**
+ * @brief Normalizes a seed to an integer in the range 0 to m - 1
+ *
+ * @param value The seed to normalize.
+ * @param modulus The modulus to use for normalization.
+ *
+ * @return The normalized seed as an integer in the range 0 to m - 1
+ */
 int32_t lehmer_seed_normalize_to_int(int32_t value, uint32_t modulus);
 
 // Generate related functions
