14abe03e0bf1063a8ada36565fb60c1a8ae7d89f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-18 22:44:32 -0400

    examples(concept): Apply patch to decouple the initialization seed from generated stream logic
    - Add a member variable to track the initial seed used to generate a new sequence
    - Add a member variable to store the sequence of generated seeds
    - Refactor the core API to handle the updated state structure
    - Apply fixes to the format specifiers
    - Add updates to the usage example

--- examples/concept.c
+++ examples/concept.c
@@ -5,7 +5,7 @@
  *
  * @brief Minimal conceptual example showcasing the core API.
  *
- * @note gcc -o concept examples/concept.c -lm
+ * @note gcc -o concept examples/concept.c -lm -g
  */
 
 #include <math.h>
@@ -22,9 +22,10 @@
 #define LEHMER_SIZE 10 // Number of values to generate
 
 typedef struct LehmerState {
-    int32_t* seed; // Array of seeds representing the initial state
-    uint32_t size; // The number of seeds (upper limit)
-    uint32_t index; // The selected seed to generate sequences from
+    int32_t seed; // The initial seed used to start the sequence
+    int32_t* stream; // The sequence of generated integers
+    uint32_t size; // The number of values to generate
+    uint32_t index; // The current index in the sequence
 } lehmer_state_t;
 
 /**
@@ -50,42 +51,43 @@ int32_t lehmer_generate_modulo(int32_t z) {
     const uint32_t m = LEHMER_MODULUS;
 
     // Lehmer formula: (a * z) % m
-    int64_t temp = (int64_t) a * z; // Use int64_t to avoid overflow
-    int32_t r = (int32_t) (temp % m); // Modulus operation
+    int64_t scale = (int64_t) a * z; // Use int64_t to avoid overflow
+    int32_t r = (int32_t) (scale % m); // Modulus operation
     return lehmer_seed_normalize_to_int(r, m);
 }
 
-// Generalized Lehmer sequence generator
+// Generate the sequence and store it in the stream array
 void lehmer_generate(
     lehmer_state_t* state, lehmer_generate_t generator, int32_t seed
 ) {
-    // Initialize the first seed
-    state->seed[0] = seed;
+    // Set the initial seed
+    state->seed = seed;
+    state->stream[0] = generator(seed);
 
-    // Generate the sequence of seeds
+    // Generate the sequence and store it in the stream array
     for (uint32_t i = 1; i < state->size; i++) {
-        // Use the previous seed to generate the next
-        int32_t previous = state->seed[i - 1];
-        state->seed[i] = generator(previous);
+        state->stream[i] = generator(state->stream[i - 1]);
     }
 }
 
 // Create and initialize the state with dynamic seed generation
-lehmer_state_t* lehmer_state_create(uint32_t size, int32_t seed) {
+lehmer_state_t* lehmer_state_create(int32_t seed, uint32_t size) {
     // Allocate memory for managing the LCG PRNG state
     lehmer_state_t* state = (lehmer_state_t*) malloc(sizeof(lehmer_state_t));
     if (NULL == state) {
         return NULL;
     }
 
-    // Initialize the index to 0
+    // Zero-initialize the index
     state->index = 0;
+    // Default to a seed of 123456789 if seed is 0
+    state->seed = (0 >= seed) ? LEHMER_SEED : seed;
     // Default to a size of 256 if size is 0
     state->size = (0 >= size) ? LEHMER_SIZE : size;
 
     // Allocate memory for generating seeds
-    state->seed = (int32_t*) malloc(sizeof(int32_t) * state->size);
-    if (NULL == state->seed) {
+    state->stream = (int32_t*) malloc(sizeof(int32_t) * state->size);
+    if (NULL == state->stream) {
         free(state);
         return NULL;
     }
@@ -99,8 +101,8 @@ lehmer_state_t* lehmer_state_create(uint32_t size, int32_t seed) {
 // Free the allocated memory
 void lehmer_state_free(lehmer_state_t* state) {
     if (state) {
-        if (state->seed) {
-            free(state->seed);
+        if (state->stream) {
+            free(state->stream);
         }
         free(state);
     }
@@ -111,14 +113,17 @@ void lehmer_state_free(lehmer_state_t* state) {
 void lehmer_state_print(lehmer_state_t* state) {
     fprintf(stderr, "lehmer->size: %zu\n", state->size);
     fprintf(stderr, "lehmer->index: %zu\n", state->index);
-    fprintf(stderr, "lehmer->seed:");
+    fprintf(stderr, "lehmer->seed: %zu\n", state->seed);
+    fprintf(stderr, "lehmer->stream:");
+
     // print the first 10 seeds or size... whichever is less.
     uint32_t boundary = (state->size > 10) ? 10 : state->size;
     for (uint32_t i = 0; i < boundary; i++) {
-        fprintf(stderr, " %lu,", state->seed[i]);
+        fprintf(stderr, " %d,", state->stream[i]);
     }
+
     if (state->size > boundary) {
-        fprintf(stderr, " ..., %u - 1\n", state->size);
+        fprintf(stderr, " ..., %lu - 1\n", state->size);
     } else {
         fprintf(stderr, " ...\n");
     }
@@ -130,7 +135,6 @@ void lehmer_state_print(lehmer_state_t* state) {
 // @ref python -m lehmer.cli -v -i 10 -r 0
 // `python -m lehmer.cli -h` for more info
 int32_t expected_stream[LEHMER_SIZE] = {
-    LEHMER_SEED,
     115541394,
     283598515,
     1523151587,
@@ -140,14 +144,14 @@ int32_t expected_stream[LEHMER_SIZE] = {
     1426670162,
     1289797906,
     2136310349,
-    // 1819611286,
+    1819611286,
 };
 
 #define LEHMER_ASSERT_INTEGER(iteration, expected, current) \
     if ((expected) != (current)) { \
         fprintf( \
             stderr, \
-            "Iteration %u: Expected %lu, Got %lu\n", \
+            "Iteration %zu: Expected %d, Got %d\n", \
             iteration, \
             expected, \
             current \
@@ -159,7 +163,7 @@ int32_t expected_stream[LEHMER_SIZE] = {
     if (!((fabsf(expected - current)) < (0.000001f))) { \
         fprintf( \
             stderr, \
-            "Iteration %u: Expected %f, Got %f\n", \
+            "Iteration %zu: Expected %.7f, Got %.7f\n", \
             iteration, \
             expected, \
             current \
@@ -169,23 +173,23 @@ int32_t expected_stream[LEHMER_SIZE] = {
 
 int main(void) {
     // Initialize the Lehmer RNG state
-    lehmer_state_t* state = lehmer_state_create(LEHMER_SIZE, LEHMER_SEED);
+    lehmer_state_t* state = lehmer_state_create(LEHMER_SEED, LEHMER_SIZE);
     lehmer_state_print(state);
 
     // Correct the index handling, ensuring the expected seeds match
     for (uint32_t i = 0; i < state->size; i++) {
         state->index = i;
-        printf("Iteration %u: Seed = %d\n", i + 1, state->seed[i]);
-        LEHMER_ASSERT_INTEGER(i, expected_stream[i], state->seed[i]);
+        printf("Iteration %zu: Seed = %d\n", i + 1, state->stream[i]);
+        LEHMER_ASSERT_INTEGER(i, expected_stream[i], state->stream[i]);
     }
 
     // Normalize the last seed to get the floating-point random number
-    int32_t last_seed = state->seed[state->size - 1];
+    int32_t last_seed = state->stream[state->size - 1];
     float random = lehmer_seed_normalize_to_float(last_seed);
-    printf("Random Number = %f\n", random);
+    printf("Random Number = %.7f\n", random);
 
     // Compare with the expected value (adjust based on corrected expectations)
-    float expected = 0.9947970276674242f;
+    float expected = 0.8473225342330162f;
     LEHMER_ASSERT_FLOAT(state->index, expected, random);
 
     return 0;
